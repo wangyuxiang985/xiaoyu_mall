adb4412ee2f4818236651d7183279df0f4213314
wangyu <wangyuxiang985@gmail.com>
2021-12-27 23:10:02 +0800

    feat(*): 添加gateway网管

---
+++ yumall-gateway/src/test/java/com/wyx/yumall/gateway/YumallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyx.yumall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
